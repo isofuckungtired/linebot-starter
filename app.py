b32dafe34464339bd26483805b2ca1f6cc5f0608
isofuckungtired <153265064+isofuckungtired@users.noreply.github.com>
2026-06-09 22:09:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from google import genai
 
 app = Flask(__name__)
 
-LINE_CHANNEL_SECRET = os.environ.get('a5dd19ca75f3aa7164172a9c4729bed1')
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('bmFS85/iNWUXXIBoaH1fF4ovfQfuuGv8U282WwOVP0yUUrCjAC0Gr/hLDtKm1zJwTyISbgPwS8rUSdOn7Za7VtCfbW361iceHASe5BkBaqGQ7OLZaSxyxldqUEsqIkb7KuqlOmmPbrd/b90UR6wfrwdB04t89/1O/w1cDnyilFU=')
-GEMINI_API_KEY = os.environ.get('AIzaSyCh-eTHZEe1GyhfpYG0j9yxe7M6APEMPls')
+LINE_CHANNEL_SECRET = ('a5dd19ca75f3aa7164172a9c4729bed1')
+LINE_CHANNEL_ACCESS_TOKEN = ('bmFS85/iNWUXXIBoaH1fF4ovfQfuuGv8U282WwOVP0yUUrCjAC0Gr/hLDtKm1zJwTyISbgPwS8rUSdOn7Za7VtCfbW361iceHASe5BkBaqGQ7OLZaSxyxldqUEsqIkb7KuqlOmmPbrd/b90UR6wfrwdB04t89/1O/w1cDnyilFU=')
+GEMINI_API_KEY = ('AIzaSyCh-eTHZEe1GyhfpYG0j9yxe7M6APEMPls')
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
